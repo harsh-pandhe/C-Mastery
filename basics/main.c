060672b0a8b817dddb79b4fbdd6cf12d164dd4f4
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-25 00:36:10 +0530

    Variable Initialization

--- basics/main.c
+++ basics/main.c
@@ -5,10 +5,15 @@ int main()
     printf("Hello, World!\n");
 
     // Variables
-    int i, j;
-    char c;
-    float f;
-    double d;
+    int i = 10, j = 20;
+    char c = 'H';
+    float f = 10.5;
+    double d = 100.44446;
+
+    printf("i = %d, j = %d\n", i, j);
+    printf("c = %c\n", c);
+    printf("f = %f\n", f);
+    printf("d = %lf\n", d);
 
     return 0;
 }

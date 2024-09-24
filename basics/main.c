ae93b283dae8635323b551fc565be5bec0ffeb1b
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-25 01:43:15 +0530

    Arrays

--- basics/main.c
+++ basics/main.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
-
+#define SIZE 10
 int main()
 {
-    // Ternary operator
-    int a = 10, b = 20, c;
-    c = (a > b) ? a : b;
+    int arr[SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        arr[i] = i;
+    }
 
-    printf("The greater number is %d\n", c);    
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
 
+    int a[] = {1, 2, 3, 4, 5};
+    printf("%d\n", a[0]);
+
+    int b[5] = {1, 2, 3, 4, 5};
+    printf("%d\n", b[0]);
+
+    b[5] = 6;
+    printf("%d\n", b[5]);
+
+    int z[3][4];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            z[i][j] = i + j;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ", z[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }

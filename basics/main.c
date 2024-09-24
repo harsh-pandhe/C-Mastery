ec38ccb81c3e5af12972a7809e23d64c647a3f86
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-25 00:45:37 +0530

    Ternary Operator

--- basics/main.c
+++ basics/main.c
@@ -2,111 +2,11 @@
 
 int main()
 {
-    // Operators
-    // Arithmetic operators
+    // Ternary operator
+    int a = 10, b = 20, c;
+    c = (a > b) ? a : b;
 
-    int i = 100, j = 20, k;
-    k = i + j;
-    printf("Addition: %d\n", k);
-
-    k = i - j;
-    printf("Subtraction: %d\n", k);
-
-    k = i * j;
-    printf("Multiplication: %d\n", k);
-
-    k = i / j;
-    printf("Division: %d\n", k);
-
-    k = i % j;
-    printf("Modulus: %d\n", k);
-
-    // Relational operators
-
-    int a = 10, b = 20;
-    if (a == b)
-    {
-        printf("a is equal to b\n");
-    }
-    else
-    {
-        printf("a is not equal to b\n");
-    }
-
-    if (a != b)
-    {
-        printf("a is not equal to b\n");
-    }
-    else
-    {
-        printf("a is equal to b\n");
-    }
-
-    if (a > b)
-    {
-        printf("a is greater than b\n");
-    }
-    else
-    {
-        printf("a is not greater than b\n");
-    }
-
-    if (a < b)
-    {
-        printf("a is less than b\n");
-    }
-    else
-    {
-        printf("a is not less than b\n");
-    }
-
-    if (a >= b)
-    {
-        printf("a is greater than or equal to b\n");
-    }
-    else
-    {
-        printf("a is not greater than or equal to b\n");
-    }
-
-    if (a <= b)
-    {
-        printf("a is less than or equal to b\n");
-    }
-    else
-    {
-        printf("a is not less than or equal to b\n");
-    }
-
-    // Logical operators
-
-    int x = 1, y = 0;
-    if (x && y)
-    {
-        printf("Both x and y are true\n");
-    }
-    else
-    {
-        printf("Either x or y is false\n");
-    }
-
-    if (x || y)
-    {
-        printf("Either x or y is true\n");
-    }
-    else
-    {
-        printf("Both x and y are false\n");
-    }
-
-    if (!x)
-    {
-        printf("x is false\n");
-    }
-    else
-    {
-        printf("x is true\n");
-    }
+    printf("The greater number is %d\n", c);    
 
     return 0;
 }

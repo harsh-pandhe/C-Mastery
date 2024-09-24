240f04c30e70bfc5b872c6670b13822ba0060621
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-25 00:43:07 +0530

    Operators

--- basics/main.c
+++ basics/main.c
@@ -2,18 +2,111 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    // Operators
+    // Arithmetic operators
 
-    // Variables
-    int i = 10, j = 20;
-    char c = 'H';
-    float f = 10.5;
-    double d = 100.44446;
+    int i = 100, j = 20, k;
+    k = i + j;
+    printf("Addition: %d\n", k);
 
-    printf("i = %d, j = %d\n", i, j);
-    printf("c = %c\n", c);
-    printf("f = %f\n", f);
-    printf("d = %lf\n", d);
+    k = i - j;
+    printf("Subtraction: %d\n", k);
+
+    k = i * j;
+    printf("Multiplication: %d\n", k);
+
+    k = i / j;
+    printf("Division: %d\n", k);
+
+    k = i % j;
+    printf("Modulus: %d\n", k);
+
+    // Relational operators
+
+    int a = 10, b = 20;
+    if (a == b)
+    {
+        printf("a is equal to b\n");
+    }
+    else
+    {
+        printf("a is not equal to b\n");
+    }
+
+    if (a != b)
+    {
+        printf("a is not equal to b\n");
+    }
+    else
+    {
+        printf("a is equal to b\n");
+    }
+
+    if (a > b)
+    {
+        printf("a is greater than b\n");
+    }
+    else
+    {
+        printf("a is not greater than b\n");
+    }
+
+    if (a < b)
+    {
+        printf("a is less than b\n");
+    }
+    else
+    {
+        printf("a is not less than b\n");
+    }
+
+    if (a >= b)
+    {
+        printf("a is greater than or equal to b\n");
+    }
+    else
+    {
+        printf("a is not greater than or equal to b\n");
+    }
+
+    if (a <= b)
+    {
+        printf("a is less than or equal to b\n");
+    }
+    else
+    {
+        printf("a is not less than or equal to b\n");
+    }
+
+    // Logical operators
+
+    int x = 1, y = 0;
+    if (x && y)
+    {
+        printf("Both x and y are true\n");
+    }
+    else
+    {
+        printf("Either x or y is false\n");
+    }
+
+    if (x || y)
+    {
+        printf("Either x or y is true\n");
+    }
+    else
+    {
+        printf("Both x and y are false\n");
+    }
+
+    if (!x)
+    {
+        printf("x is false\n");
+    }
+    else
+    {
+        printf("x is true\n");
+    }
 
     return 0;
 }

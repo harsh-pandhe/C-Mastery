fa3fe03b3040a623cfd74becc6d21c189fa6f8e5
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-25 17:34:48 +0530

    C++ Configuration

--- basics/main.c
+++ basics/main.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 
-int funct(int a, int b)
-{
-    return a + b;
-}
-
-unsigned int factorial(unsigned int n)
-{
-    if (n == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return n * factorial(n - 1);
-    }
-}
-
 int main()
 {
-    int c = funct(5, 6);
-    printf("%d\n", c);
 
-    unsigned int n = 5;
-    printf("Factorial of %d is %d\n", n, factorial(n));
     return 0;
 }
